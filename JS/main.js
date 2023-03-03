2ae8a76afeae76d415c28e9f9aa235c15e5f852e
sang ah <kr.sangah@gmail.com>
2023-03-03 15:01:42 +0900

    230303

--- JS/main.js
+++ JS/main.js
@@ -34,8 +34,23 @@ function controlAutoPlay() {
 
 // ARROW 클릭해서 스크롤 페이지 이동
 const park981 = document.querySelector('.park981');
+const runfarm = document.querySelector('.letsrunfarm');
+const kitesurfing = document.querySelector('.kitesurfing');
+const blueone = document.querySelector('.blueone');
+const bouncesuper = document.querySelector('.bouncesuper');
+
 const park981Height = 1066;
+const runfarmHeight = 2958;
+const kitesurfHeight = 5172;
+const blueHeight = 7418;
+const bounceHeight = 9739;
+
+
 const firstarrow = document.querySelector('.firstarrow')
+const secondarrow = document.querySelector('.secondarrow')
+const thirdarrow = document.querySelector('.thirdarrow')
+const fourtharrow = document.querySelector('.fourtharrow')
+const fiftharrow = document.querySelector('.fiftharrow')
 
 let firstarrowClick = function () {
   window.scroll({
@@ -45,5 +60,45 @@ let firstarrowClick = function () {
   })
 }
 
+let secondarrowClick = function () {
+  window.scroll({
+    behavior: "smooth",
+    top: runfarmHeight,
+    left: 0
+  })
+}
+
+let thirdarrowClick = function () {
+  window.scroll({
+    behavior: "smooth",
+    top: kitesurfHeight,
+    left: 0
+  })
+}
+
+let fourtharrowClick = function () {
+  window.scroll({
+    behavior: "smooth",
+    top: blueHeight,
+    left: 0
+  })
+}
+
+let fiftharrowClick = function () {
+  window.scroll({
+    behavior: "smooth",
+    top: bounceHeight,
+    left: 0
+  })
+}
+
 firstarrow.addEventListener("click", firstarrowClick)
+secondarrow.addEventListener("click", secondarrowClick)
+thirdarrow.addEventListener("click", thirdarrowClick)
+fourtharrow.addEventListener("click", fourtharrowClick)
+fiftharrow.addEventListener("click", fiftharrowClick)
+
+
+
+
 
